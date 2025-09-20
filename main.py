14aaebb9ce925e3694946c195532531f79f708be
justinkmonkey <justinkmonkey@hotmail.com>
2025-09-20 16:18:43 -0500

    rocket moving and code organized

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import pygame
 import sys
 from settings import Settings
-
-
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_hight))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = "blue"
-   
+  
+    ship = Ship(screen)
     # Start the main loop for the game.
     while True:
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 run_game()
